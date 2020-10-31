809fd711496ab2a9ca3f097d272ad758aabe7aec
jungbum <jungbumwoo@naver.com>
2020-10-31 23:26:47 +0900

    #3.4 SendMail works!!

--- server/src/utils.js
+++ server/src/utils.js
@@ -2,32 +2,32 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 import { adjectives, noun} from "./words";
-import nodemailer from "nodemailer";
-import sgTransport from "nodemailer-sendgrid-transport";
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SG_API);
 
 export const secretGenerator = () => {
     let randomNum = Math.floor(Math.random() * adjectives.length);
     return `${adjectives[randomNum]} ${noun[randomNum]}`
 }
 
-
-const sendMail = email => {
-    const options = {
-        auth: {
-            api_user: process.env.SGID,
-            api_key: process.env.SGPASSWORD
+const sendMail = async(email) => {
+    try {
+        await sgMail.send(email);
+        console.log("mail is sent!");
+    } catch(error) {
+        console.log(error);
+        if (error.response) {
+            console.error(error.response.body)
         }
-    };
-    const client = nodemailer.createTransport(sgTransport(options));
-    return client.sendMail(email);
+    }
 }
-
 
 export const sendSecretMail = (address, secret) => {
     const email = {
         from: "bumbum@prismagramfood.co",
         to: address,
         subject: "🔒Login Secret for Prismagram🔒",
+        text: "lala blah blah",
         html: `Hello! Your login secret is ${secret}.<br/>Copy paste on the app/website to log in`
     };
     return sendMail(email);
